6ae39774d895f44e84bdfec2b56535cea22215bc
ldf <leeka9504@gmail.com>
2022-10-27 23:52:07 +0900

    main

--- main.py
+++ main.py
@@ -9,6 +9,8 @@ cors = CORS(app, resources={r"/*": {"origins": "*"}})
 
 @app.route("/hello")
 def hello():
-
     result = {"code": 200, "message": "jenkins test"}
     return result
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=80)
